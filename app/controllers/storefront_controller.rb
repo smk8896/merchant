81c50ed0e56e767c72849076bedf287eb27bb049
Shea Matthew Kennisher <deidrelarroque@Deidres-MacBook-Air.local>
2017-06-21 10:32:54 -0500

    Add ability to shop by category *Change Navbar *Shop dropdown with dynamic categories *Update storefront controller to handle prodict filtering *Change Storefront Index View to Show Current Category

--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -1,6 +1,12 @@
 class StorefrontController < ApplicationController
   def index
-    @products = Product.all
+    if params[:cat_id]
+      # I only want to see the stuff in that category
+      @category = Category.find(params[:cat_id])
+      @products = Product.where(category_id: params[:cat_id])
+    else
+      @products = Product.all
+    end
   end
 
   def about
